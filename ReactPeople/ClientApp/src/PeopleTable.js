79ffc34e1f4561582b04f9e68b621395462e5fa0
Hudi Guttman <hudiguttman@gmail.com>
2021-04-25 15:09:26 -0400

    Made Changes

--- ReactPeople/ClientApp/src/PeopleTable.js
+++ ReactPeople/ClientApp/src/PeopleTable.js
@@ -7,20 +7,18 @@ import PersonForm from './PersonForm.js';
 
 class PeopleTable extends React.Component{
   state = {
-      person:{
           firstName:'',
           lastName:'',
-          age:''
-      },
+          age:'',
       people: []
   }
 onAddClick = () =>{
-    const { firstName, lastName, age, People } = this.state;
+    const { firstName, lastName, age, people } = this.state;
     const person = {
         firstName, lastName, age
     };
-    const peopleCopy = [...People, person];
-    this.setState({ People: peopleCopy, firstName: '', lastName: '', age: '' });
+    const peopleCopy = [...people, person];
+    this.setState({ people: peopleCopy, firstName: '', lastName: '', age: '' });
 }
 
   onFirstNameChange = e =>{
@@ -36,48 +34,52 @@ onAgeChange = e =>{
     this.setState({age});
 };
 onClearClick = () =>{
-    this.setState({people: [] });
+    const empty = [];
+    this.setState({ People: empty });
 };
-FillTable = () =>{
-    if(this.state.people.count == 0){
-        return <h2>No people added yet...</h2>
-    }
-    else{
-        return
-        <table className = "table-hover table-bordered table-striped">
-            <thead>
-                <tr>
-                   <th>First Name</th>
-                   <th>Last Name</th>
-                   <th>Age</th>
-                </tr>
-            </thead>
-            <tbody>
-                {this.state.people.map((person, idx) => <PersonRow
-                firstName = {person.firstName}
-                lastName = {person.lastName}
-                age = {person.age}
-                index = {idx} /> )}
-            </tbody>
-        </table>
-    }
-}
+    FillTable = () => {
+        if (this.state.people.length == 0) {
+            return <h1>No people added yet...</h1>
+        }
+        else {
+            return  <table className = "table table-hover table-bordered table-striped mt-3" >
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Age</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.state.people.map((person, idx) => <PersonRow
+                        firstName={person.firstName}
+                        lastName={person.lastName}
+                        age={person.age}
+                        index={idx} />)}
+                </tbody>
+            </table >
+                }
+           }
 
-  render(){
-    return(
-        <div className = 'container'>
-            <PersonForm
-            person = {this.state.person}
-            onFirstNameChange = {this.onFirstNameChange}
-            onLastNameChange = {this.onLastNameChange}
-            onAgeChange = {this.onAgeChange}
-            onAddClick ={this.onAddClick}
-            onClearClick = {this.onClearClick}
-            />
-            {this.FillTable()}
-        </div>
-    )
+    render() {
+        return (
+            <div className='container'>
+                <PersonForm
+                    firstName={this.state.firstName}
+                    lastName={this.state.lastName}
+                    age={this.state.age}
+                    onFirstNameChange={this.onFirstNameChange}
+                    onLastNameChange={this.onLastNameChange}
+                    onAgeChange={this.onAgeChange}
+                    onAddClick={this.onAddClick}
+                    onClearClick={this.onClearClick}
+                />
+                <div className='container'>
+                    {this.FillTable()}
+                </div>
+            </div>
+        );
+    }
   }
 
-}
 export default PeopleTable;
--- ReactPeople/ClientApp/src/PersonRow.js
+++ ReactPeople/ClientApp/src/PersonRow.js
@@ -2,16 +2,23 @@ import React from 'react';
 
 
 class PersonRow extends React.Component{
-    render(){
-        return(
-            <div>
-               <tr key = {this.props.index} className={this.props.age > 65 ? 'table-danger': '' }>
-                   <td>{this.props.firstName}</td>
-                   <td>{this.props.lastName}</td>
-                   <td>{this.props.age}</td>
-               </tr>
-            </div>
-        )
+    render() {
+        if (this.props.firstName == null && this.props.lastName == null && this.props.age == null) {
+            return (
+                <h1>No people added yet...</h1>
+            )
+        }
+        else {
+            return (
+                <div>
+                    <tr key={this.props.index} className={this.props.age > 65 ? 'table-danger' : ''}>
+                        <td>{this.props.firstName}</td>
+                        <td>{this.props.lastName}</td>
+                        <td>{this.props.age}</td>
+                    </tr>
+                </div>
+            );
+        }
     }
 }
 export default PersonRow;
